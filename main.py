20e72bf5c97b5daba688c021b38a2f8b2c3af174
isantino <santino.medina209@gmail.com>
2024-10-10 20:58:27 +0000

    cree variable y habilitación para votar

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-name="santino"
+name="Santino"
+print(name) 
+number=1
+booleano=True
+booleano=False 
+print(number) 
+print(1) 
+edadDeSantino=15
+edadParaVotar=16
+puedeVotar= edadParaVotar<=edadDeSantino
+  if puedeVotar: 
+    print("puede votar") 
